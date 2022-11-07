d818b89a8aa7e2c77dd967a2232080c4a6a18c77
CWJWANJING <wanjingchencwj@gmail.com>
2022-11-07 22:25:04 +0000

    Added more UI elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Navigation from "../components/Navigation"
 import Me from "../components/Me"
 import Styled from "@emotion/styled"
 import "../styles/global.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const P = Styled.p`
   margin: 2% 0%;
@@ -22,6 +23,36 @@ export default function home() {
       <h1 style={style}>Wanjing Chen</h1>
       <Me />
       <P>Hi! My name is Wanjing. I'm a software developer who draws. Nice to meet you! 😊</P>
+      
+      <div class="container">
+        <h2 class="text-center">Projects</h2>
+        <div class="row">
+          <div class="col-lg-4 card">
+            <img class="card-img-top" src="..." alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="col-lg-4 card">
+            <img class="card-img-top" src="..." alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="col-lg-4 card">
+            <img class="card-img-top" src="..." alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
