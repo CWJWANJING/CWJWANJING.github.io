3f96762b95915e9dbd4117c1373f0bd20da05b08
CWJWANJING <wanjingchencwj@gmail.com>
2022-11-08 21:27:29 +0000

    Styled cards and added links

---
+++ src/components/Projects.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import "../styles/global.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Projects = () => {
+    return (
+        <div className="container">
+                <h2 className="text-center">Projects</h2>
+                <div style={{textAlign: 'right', paddingBottom: '2%'}}>
+                    <a className="text-right" href="/projects">More projects</a>
+                </div>
+                <div className="row">
+                <div className="col-lg-4 card">
+                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div className="col-lg-4 card">
+                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div className="col-lg-4 card">
+                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                </div>
+            </div>
+    )}
+
+export default Projects;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 import Navigation from "../components/Navigation"
 import Me from "../components/Me"
+import Projects from "../components/Projects"
+import Blogs from "../components/Blogs"
 import Styled from "@emotion/styled"
 import "../styles/global.css"
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -23,36 +25,8 @@ export default function home() {
       <h1 style={style}>Wanjing Chen</h1>
       <Me />
       <P>Hi! My name is Wanjing. I'm a software developer who draws. Nice to meet you! 😊</P>
-      
-      <div class="container">
-        <h2 class="text-center">Projects</h2>
-        <div class="row">
-          <div class="col-lg-4 card">
-            <img class="card-img-top" src="..." alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="col-lg-4 card">
-            <img class="card-img-top" src="..." alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-          <div class="col-lg-4 card">
-            <img class="card-img-top" src="..." alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Blogs />
+      <Projects />
     </>
   )
 }
