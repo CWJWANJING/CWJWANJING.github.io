96da604a76af021bc5c384362383404175c831d3
CWJWANJING <wanjingchencwj@gmail.com>
2023-01-24 21:48:38 +0000

    Has previous and next article

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -1,5 +1,7 @@
 import * as React from "react"
+import { getSrc } from "gatsby-plugin-image"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import SEO from "gatsby-theme-blog/src/components/seo"
 import Layout from "gatsby-theme-blog/src/components/layout"
 import PostTitle from "gatsby-theme-blog/src/components/post-title"
 import PostDate from "gatsby-theme-blog/src/components/post-date"
@@ -18,24 +20,32 @@ const Post = ({
   previous,
   next,
 }) => (
-    <>
+  <>
     <Navigation />
     <Layout location={location} title={title}>
-        <main>
-            <article>
-                <header>
-                    <PostHero post={post} />
-                    <PostTitle>{post.title}</PostTitle>
-                    <PostDate>{post.date}</PostDate>
-                </header>
-                <section>
-                    <MDXRenderer>{post.body}</MDXRenderer>
-                </section>
-            </article>
-        </main>
-        <PostFooter {...{ previous, next }} />
+      <SEO
+        title={post.title}
+        description={post.excerpt}
+        imageSource={
+          post.socialImage ? getSrc(post.socialImage) : getSrc(post.image)
+        }
+        imageAlt={post.imageAlt}
+      />
+      <main>
+        <article>
+          <header>
+            <PostHero post={post} />
+            <PostTitle>{post.title}</PostTitle>
+            <PostDate>{post.date}</PostDate>
+          </header>
+          <section>
+            <MDXRenderer>{post.body}</MDXRenderer>
+          </section>
+        </article>
+      </main>
+      <PostFooter {...{ previous, next }} />
     </Layout>
-    </>
+  </>
 )
 
 export default Post
