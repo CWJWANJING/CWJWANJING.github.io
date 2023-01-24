2b73c46243d0cbfd923def1003e2a101fb434c33
CWJWANJING <wanjingchencwj@gmail.com>
2023-01-24 13:15:08 +0000

    refactored a bit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "../styles/global.css"
+import { Link } from 'gatsby';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/global.css";
+
+const navItems = [
+  {text: "Home", link: "/"},
+  {text: "Projects", link: "/projects"},
+  {text: "Arts", link: "/art"},
+  {text: "Blog", link: "/blog"},
+]
 
 const Navigation = () => {
+  const renderedNavItems = navItems.map((navItem) => 
+    <li className="nav-item" key={navItem.link}>
+      <Link className="nav-link" to={navItem.link}>{navItem.text}</Link>
+    </li>
+  );
+  
   return (
     <nav className="navbar navbar-expand-lg" style={{backgroundColour: "#4881ea"}}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Wanjing Logo</a>
+        <Link to="/">Wanjing logo XD</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/projects">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/art">Art</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/blog">Blog</a>
-            </li>
+            {renderedNavItems}
           </ul>
         </div>
       </div>
