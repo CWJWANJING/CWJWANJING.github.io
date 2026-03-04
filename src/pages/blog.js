6572fe918d86dcf8bc07788595185694815bb9f0
Wanjing Chen <wanjingchencwj@gmail.com>
2026-03-04 11:08:23 +0000

    Added navigation for blogs and blog list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
+import Navigation from "../components/Navigation";
 import "../styles/blog-list.css";
 
 export default function Blog({ data }) {
   return (
-    <main className="blog-list-container">
-      <h1>My Blogs</h1>
-      {data.allMarkdownRemark.nodes.map((post) => (
-        <article key={post.id} className="blog-list-item">
-          <h2>
-            <Link to={post.frontmatter.slug || `/blog/${post.id}`}>
-              {post.frontmatter.title}
-            </Link>
-          </h2>
-          <p className="blog-list-date">{post.frontmatter.date}</p>
-          <p className="blog-list-excerpt">{post.excerpt}</p>
-        </article>
-      ))}
-    </main>
+    <div>
+      <Navigation />
+      <main className="blog-list-container">
+        <h1>My Blogs</h1>
+        {data.allMarkdownRemark.nodes.map((post) => (
+          <article key={post.id} className="blog-list-item">
+            <h2>
+              <Link to={post.frontmatter.slug || `/blog/${post.id}`}>
+                {post.frontmatter.title}
+              </Link>
+            </h2>
+            <p className="blog-list-date">{post.frontmatter.date}</p>
+            <p className="blog-list-excerpt">{post.excerpt}</p>
+          </article>
+        ))}
+      </main>
+    </div>
   );
 }
 
--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,20 +1,24 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import Navigation from "../components/Navigation"
 import "../styles/blog-post.css" // add CSS for styling
 
 export default function BlogPost({ data }) {
-    const post = data.markdownRemark
+  const post = data.markdownRemark
 
-    return (
-        <main className="blog-post-container">
-            <h1 className="blog-post-title">{post.frontmatter.title}</h1>
-            <p className="blog-post-date">{post.frontmatter.date}</p>
-            <div
-                className="blog-post-content"
-                dangerouslySetInnerHTML={{ __html: post.html }}
-            />
-        </main>
-    )
+  return (
+    <div>
+      <Navigation />
+      <main className="blog-post-container">
+        <h1 className="blog-post-title">{post.frontmatter.title}</h1>
+        <p className="blog-post-date">{post.frontmatter.date}</p>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </main>
+    </div>
+  )
 }
 
 export const query = graphql`
